bc3a9087dd201382efddf1392e22796b0f080df9
Zack Lerner <zack@zacklerner.com>
2021-03-09 16:06:38 -0500

    Make sure it takes the correct dataset

--- template/web/client-config.js
+++ template/web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
     projectId: process.env.GRIDSOME_SANITY_PROJECT_ID || '<#< sanity.projectId >#>',
-    dataset: process.env.GRIDSOME_SANITY_DATASET || '<#< sanity.projectId >#>'
+    dataset: process.env.GRIDSOME_SANITY_DATASET || '<#< sanity.dataset >#>'
   }
 }
